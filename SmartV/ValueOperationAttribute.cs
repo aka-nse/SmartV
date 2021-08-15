463874eae89749dab9167a3502584968b74ab278
GlassGrass <nakase.d1220@gmail.com>
2021-08-15 17:40:11 +0900

    temporary

---
+++ SmartV/ValueOperationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartV
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class ValueOperationAttribute : Attribute
+    {
+        public Type OperationProviderType { get; }
+
+
+        public ValueOperationAttribute(Type operationProviderType)
+        {
+
+            OperationProviderType = operationProviderType;
+        }
+    }
+}
---
+++ SmartV/VectorOperation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartV
+{
+    public static partial class VectorOperation
+    {
+    }
+}
